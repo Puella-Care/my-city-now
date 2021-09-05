49c1e1258a5392555e5fbcfd40fef37655ca4f48
LiviaMedeiros <livia@cirno.name>
2021-09-05 13:42:36 +0800

    updated qb_step

--- docs/qb/step.js
+++ docs/qb/step.js
@@ -1,17 +1,9 @@
 /*! qb_step made by 엘샤, originally drawn by f4samurai | ©Magica Quartet/Aniplex, Magia Record Partners */
-;(d => { "use strict";
-	const qb_step = (e, t = 0x3a0, c = 'qb_step', p = d.body) =>
-		setTimeout(p.removeChild.bind(p), t, (qb => {
-			qb.className = c;
-			qb.style.left = e.clientX + 'px';
-			qb.style.top = e.clientY + 'px';
-			qb.appendChild(d.createElement('div'));
-			qb.appendChild(d.createElement('div'));
-			qb.appendChild(d.createElement('div'));
-			qb.style.display = 'block';
-			return p.appendChild(qb);
-		})(d.createElement('div')));
-	return d.addEventListener(...('ontouchstart' in d.documentElement
-		? ['touchstart', e => qb_step(e.changedTouches[0])]
-		: ['mousedown', qb_step]), {passive: true});
-})(document);
+;(d => d.addEventListener('pointerdown',
+	({x, y}, t = 0x3a0, c = 'qb_step', n = 3, p = d.body) =>
+	setTimeout(p.removeChild.bind(p), t, (qb => {
+		for (qb.className = c; n--; qb.appendChild(d.createElement('div')));
+		Object.assign(qb.style, {left:x+'px', top:y+'px', display:'block'});
+		return p.appendChild(qb);
+	})(d.createElement('div'))), {passive:true})
+)(document);
